4c257d30e31212fb3d7b98c167e0e26601a50924
havenfricke <havenfricke@u.boisestate.edu>
2026-04-01 11:02:17 -0600

    removed desc from models as it is a reserved keyword in MySQL

--- models/Example.py
+++ models/Example.py
@@ -1,7 +1,7 @@
 class Example:
-    def __init__(self, id: int, name: str, desc: str, created_at: str):
+    def __init__(self, id: int, name: str, description: str, created_at: str):
         self.id = id
         # self.creator_id = creator_id Add this when auth is integrated
         self.name = name
-        self.description = desc
+        self.description = description
         self.created_at = created_at
